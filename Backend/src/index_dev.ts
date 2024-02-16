9ae4e7cd04079b98714b187b197110c23a17a0c0
ryy197340 <ryy197340@gmail.com>
2024-02-16 06:43:28 -0800

    test

--- Backend/src/index_dev.ts
+++ Backend/src/index_dev.ts
@@ -1,24 +1,23 @@
-import { createConnection } from "typeorm";
-import express from "express";
-import * as bodyParser from "body-parser";
-import cors from 'cors';
+import { createConnection } from 'typeorm'
+import express from 'express'
+import * as bodyParser from 'body-parser'
+import cors from 'cors'
 
-import * as dotenv from 'dotenv';
+import * as dotenv from 'dotenv'
 dotenv.config({
-    path: './.env.dev'
-});
+  path: './.env.dev',
+})
 
-import logger from "./utils/logger";
-import { PORT_NUMBER } from "./config";
-const  { router } = require("./services");
-
+import logger from './utils/logger'
+import { PORT_NUMBER } from './config'
+import router from './services/routes'
 
 // create express app
-const app = express();
-app.use(bodyParser.json());
-app.use(cors());
-app.set('/', router);
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/', router)
 
-app.listen(PORT_NUMBER);
+app.listen(PORT_NUMBER)
 
-console.log(`Solmate Presale API has started on ${PORT_NUMBER} port.`);
+console.log(`Solmate Presale API has started on ${PORT_NUMBER} port.`)
--- Backend/src/services/index.ts
+++ Backend/src/services/index.ts
@@ -1,1 +1,1 @@
-export * from './routes';
+export { router } from './routes'
